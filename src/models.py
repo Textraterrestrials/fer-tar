362c69f4e305d232317d95a0ec066912aab44215
maculjak <mculyak@gmail.com>
2021-05-16 21:09:16 +0200

    update models.py

--- src/models.py
+++ src/models.py
@@ -2,7 +2,11 @@ import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
+from torch.utils.data import DataLoader
 from transformers import BertModel, AlbertModel, ElectraModel, RobertaModel
+
+from my_scripts import data
+from src import encoders
 
 
 class TransformerBasedClassifier(pl.LightningModule):
@@ -31,7 +35,7 @@ class TransformerBasedClassifier(pl.LightningModule):
         optimizer = optim.AdamW(self.parameters(),
                                 lr=self.hyperparams.get('lr', 2e-5),
                                 eps=self.hyperparams.get('eps', 1e-8),
-                                weight_decay=self.hyperparams.get('weight_decay', 0.01))
+                                weight_decay=self.hyperparams.get('weight_decay', 0.02))
         scheduler = optim.lr_scheduler.ExponentialLR(optimizer,
                                                      gamma=self.hyperparams.get('gamma', 1 - 1e-4))
         return {'optimizer': optimizer, 'lr_scheduler': scheduler}
@@ -57,13 +61,27 @@ class TransformerBasedClassifier(pl.LightningModule):
 class BertClassifier(TransformerBasedClassifier):
 
     def __init__(self, name='bert-base-uncased', **hyperparams):
-        super(BertClassifier, self).__init__(BertModel.from_pretrained(name), **hyperparams)
+        super(BertClassifier, self).__init__(BertModel.from_pretrained(name), embedding_size=768, **hyperparams)
 
     def embed(self, sent_batch):
         return self.transformer(
             input_ids=sent_batch['input_ids'],
             attention_mask=sent_batch['attention_mask']
         )[1]
+
+
+class RobertaClassifier(TransformerBasedClassifier):
+    def __init__(self, name='roberta-base', **hyperparams):
+        super(RobertaClassifier, self).__init__(RobertaModel.from_pretrained(name), **hyperparams)
+
+    def embed(self, sent_batch):
+        outputs = self.transformer(
+            input_ids=sent_batch['input_ids'],
+            attention_mask=sent_batch['attention_mask']
+        )[1]
+
+        return outputs.pooler_output
+
 
 
 class ElectraClassifier(TransformerBasedClassifier):
@@ -90,22 +108,27 @@ class AlbertClassifier(TransformerBasedClassifier):
             input_ids=sent_batch['input_ids'],
             attention_mask=sent_batch['attention_mask']
         )
-        # print('vars(outputs)', vars(outputs))
-        # print('pooler_output.shape', outputs.pooler_output.shape)
+
         return outputs.pooler_output
 
 
-class RobertaClassifier(TransformerBasedClassifier):
-    def __init__(self, name='albert-base-v1', **hyperparams):
-        super(RobertaClassifier, self).__init__(RobertaModel.from_pretrained(name), 768, **hyperparams)
 
-    def embed(self, sent_batch):
-        outputs = self.transformer(
-            input_ids=sent_batch['input_ids'],
-            attention_mask=sent_batch['attention_mask']
-        )
-        # print('vars(outputs)', vars(outputs))
-        # print('pooler_output.shape', outputs.pooler_output.shape)
-        return outputs.pooler_output
 
-print('?')
+
+if __name__ == '__main__':
+    train_ds, val_ds, test_ds = data.ComVEDataset.from_data_folder(x_transforms=[encoders.RobertaEncoder()], path_to_data_folder='D:/git/fer-tar')
+    train_dataloader = DataLoader(train_ds, batch_size=32, shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=len(val_ds))
+
+    from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+    early_stop_callback = EarlyStopping(
+       monitor='val_acc',
+       min_delta=0.00,
+       patience=5,
+       verbose=False,
+       mode='max'
+    )
+
+    model = RobertaClassifier()
+    trainer = pl.Trainer(max_epochs=15, gpus=1, progress_bar_refresh_rate=20, callbacks=[EarlyStopping(monitor='val_loss')], checkpoint_callback=False)
+    trainer.fit(model, train_dataloader, val_dataloader)
