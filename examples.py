7a9e3015df2c25f576c244ae550c26fbdb391cd3
maculjak <mculyak@gmail.com>
2021-05-16 14:41:25 +0200

    add important code

--- examples.py
+++ examples.py
@@ -1,20 +1,29 @@
-from torch import utils
-from scripts import data
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+from my_scripts import data
 from src import encoders
+from src.models import BertClassifier, AlbertClassifier
+
 # example how to load datasets and iterate in batches
-fake_encoder = encoders.FakeEncoder()
-train_ds, dev_ds, test_ds = data.ComVEDataset.from_data_folder(
-    # change this accordingly
-    '~/Documents/tar-data',
-    x_transforms=[fake_encoder],
+# fake_encoder = encoders.FakeEncoder()
+# train_ds, dev_ds, test_ds = data.ComVEDataset.from_data_folder(
+#     # change this accordingly
+#     '~/Documents/tar-data',
+#     x_transforms=[encoders.BertEncoder],
+#     lazy=True
+# )
+
+train_ds = data.ComVEDataset.from_csv(
+    'subtaskA_data_all.csv',
+    'subtaskA_answers_all.csv',
+    x_transforms=[encoders.AlbertEncoder()],
     lazy=True
 )
+train_dataloader = DataLoader(train_ds, batch_size=32, shuffle=True)
 
-train_dataloader = utils.data.DataLoader(train_ds, batch_size=6, shuffle=True)
+model = AlbertClassifier()
+model.to('cpu')
 
-for i, batch in enumerate(train_dataloader):
-    x_batch, y_batch = batch
-    print(f"batch {i}")
-    print(x_batch)
-    print(y_batch)
-    break
+trainer = pl.Trainer(max_epochs=5, gpus=1, checkpoint_callback=False)
+trainer.fit(model, train_dataloader)
--- src/models.py
+++ src/models.py
@@ -1,46 +1,115 @@
 import pytorch_lightning as pl
-from torch import nn
-from transformers import BertModel
+import torch
+import torch.nn.functional as F
+from torch import nn, optim
+from transformers import BertModel, AlbertModel, ElectraModel, RobertaModel
 
 
-class BertClassification(pl.LightningModule):
-    def __init__(self, drop=0.0):
-        super().__init__()
-        self.bert = BertModel.from_pretrained('bert-base-uncased')
+class TransformerBasedClassifier(pl.LightningModule):
+    def __init__(self, transformer, embedding_size=None, drop=0.0, **hyperparams):
+        super(TransformerBasedClassifier, self).__init__()
+        if embedding_size is None:
+            embedding_size = transformer.config.pooler_output.shape[1]
+        self.transformer = transformer
         self.dropout = nn.Dropout(drop)
-        self.linear = nn.Linear(768, 2)
+        self.linear = nn.Linear(embedding_size, 1)
+        self.hyperparams = hyperparams
 
-    def forward(self, input_ids, token_type_ids, attention_mask):
-        _, pooled_output = self.bert(
-            input_ids=input_ids,
-            token_type_ids=token_type_ids,
-            attention_mask=attention_mask
-        )
-        return self.linear(self.dropout(pooled_output))
+    def forward(self, x_batch):
+        sent0_embedded = self.embed(x_batch[0])
+        sent0_embedded = sent0_embedded.mean(dim=2)
+
+        #print(sent0_embedded)
+        #print(sent0_embedded.shape)
+        sent1_embedded = self.embed(x_batch[1])
+        sent1_embedded = sent1_embedded.mean(dim=2)
+
+        bla = self.dropout(sent0_embedded)
+        # print(bla.shape)
+        sent0_logit = self.linear(bla).reshape(-1, 1)
+        sent1_logit = self.linear(self.dropout(sent1_embedded)).reshape(-1, 1)
+        e0 = torch.exp(sent0_logit)
+        e1 = torch.exp(sent1_logit)
+        output = e0 / (e0 + e1)
+        return output
 
     def configure_optimizers(self):
-        # optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        # return optimizer
-        pass
+        optimizer = optim.AdamW(self.parameters(),
+                                lr=self.hyperparams.get('lr', 2e-5),
+                                eps=self.hyperparams.get('eps', 1e-8),
+                                weight_decay=self.hyperparams.get('weight_decay', 0.01))
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer,
+                                                     gamma=self.hyperparams.get('gamma', 1 - 1e-4))
+        return {'optimizer': optimizer, 'lr_scheduler': scheduler}
 
     def training_step(self, train_batch, batch_idx):
-        # x, y = train_batch
-        # x = x.view(x.size(0), -1)
-        # z = self.encoder(x)
-        # x_hat = self.decoder(z)
-        # loss = F.mse_loss(x_hat, x)
-        # self.log('train_loss', loss)
-        # return loss
-        pass
+        x_batch, y_batch = train_batch
+        return F.binary_cross_entropy(self(x_batch), y_batch)
 
     def validation_step(self, val_batch, batch_idx):
-        # x, y = val_batch
-        # x = x.view(x.size(0), -1)
-        # z = self.encoder(x)
-        # x_hat = self.decoder(z)
-        # loss = F.mse_loss(x_hat, x)
-        # self.log('val_loss', loss)
+        x_batch, y_batch = val_batch
+        batch_p = self(x_batch)
+        loss = F.binary_cross_entropy(batch_p, y_batch)
+        predictions = batch_p >= 0.5
+        acc = torch.tensor((predictions == y_batch).float()).mean()
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', acc, prog_bar=True)
+        return loss
+
+    def embed(self, sent_batch):
         pass
 
 
-model = BertClassification()
+class BertClassifier(TransformerBasedClassifier):
+
+    def __init__(self, name='bert-base-uncased', **hyperparams):
+        super(BertClassifier, self).__init__(BertModel.from_pretrained(name), **hyperparams)
+
+    def embed(self, sent_batch):
+        return self.transformer(
+            input_ids=sent_batch['input_ids'],
+            attention_mask=sent_batch['attention_mask']
+        )[1]
+
+
+class ElectraClassifier(TransformerBasedClassifier):
+    def __init__(self, name='google/electra-small-discriminator', **hyperparams):
+        transformer = ElectraModel.from_pretrained(name) # transformer.config.hidden_size
+        super(ElectraClassifier, self).__init__(transformer,  embedding_size=256, **hyperparams)
+
+    def embed(self, sent_batch):
+        outputs = self.transformer(
+            input_ids=sent_batch['input_ids'],
+            attention_mask=sent_batch['attention_mask']
+        )
+
+        return outputs.last_hidden_state[2]
+
+
+class AlbertClassifier(TransformerBasedClassifier):
+
+    def __init__(self, name='albert-base-v1', **hyperparams):
+        super(AlbertClassifier, self).__init__(AlbertModel.from_pretrained(name), 768, **hyperparams)
+
+    def embed(self, sent_batch):
+        outputs = self.transformer(
+            input_ids=sent_batch['input_ids'],
+            attention_mask=sent_batch['attention_mask']
+        )
+        # print('vars(outputs)', vars(outputs))
+        # print('pooler_output.shape', outputs.pooler_output.shape)
+        return outputs.pooler_output
+
+
+class RobertaClassifier(TransformerBasedClassifier):
+    def __init__(self, name='albert-base-v1', **hyperparams):
+        super(RobertaClassifier, self).__init__(RobertaModel.from_pretrained(name), 768, **hyperparams)
+
+    def embed(self, sent_batch):
+        outputs = self.transformer(
+            input_ids=sent_batch['input_ids'],
+            attention_mask=sent_batch['attention_mask']
+        )
+        # print('vars(outputs)', vars(outputs))
+        # print('pooler_output.shape', outputs.pooler_output.shape)
+        return outputs.pooler_output
